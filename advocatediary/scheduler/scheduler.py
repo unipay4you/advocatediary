72b7a61d5b64c14b8b865d729520ae74e93819a8
unipay4you <unipay4you@gmail.com>
2025-02-21 20:49:33 +0530

    dbbackup_limit_set

--- advocatediary/scheduler/scheduler.py
+++ advocatediary/scheduler/scheduler.py
@@ -6,7 +6,7 @@ from django.core.management import call_command
 
 
 
-def backup_every_one_min():
+def daily_db_backup():
     print('Database backed up')
     call_command('dbbackup', clean=True)
 
@@ -18,12 +18,12 @@ def delete_old_job_executions(max_age = 2): # max age in seconds
 def  start():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), 'default')
-    scheduler.add_job(backup_every_one_min, 'interval', seconds=60,
+    scheduler.add_job(daily_db_backup, 'interval', minutes=180,
                       jobstore='default',
-                      id='backup_every_one_min',
+                      id='daily_db_backup',
                       replace_existing=True)
     
-    scheduler.add_job(delete_old_job_executions, 'interval', seconds=10,
+    scheduler.add_job(delete_old_job_executions, 'interval', minutes=180,
                       jobstore='default',
                       id='delete_old_job_executions',
                       replace_existing=True)

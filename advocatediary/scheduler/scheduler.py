8c8dade85ee1653bd4d4264db414535683a15d5b
unipay4you <unipay4you@gmail.com>
2025-02-22 09:40:57 +0530

    setting_after_gitignore

--- advocatediary/scheduler/scheduler.py
+++ advocatediary/scheduler/scheduler.py
@@ -4,8 +4,6 @@ from django_apscheduler.models import DjangoJobExecution
 from django_apscheduler import util
 from django.core.management import call_command
 
-
-
 def daily_db_backup():
     print('Database backed up')
     call_command('dbbackup', clean=True)
